8b296b1f21534684c1a436c99bd473b2832f6951
chelo tommy <140934408+chelotommy@users.noreply.github.com>
2023-07-30 20:39:30 +0530

    Delete script.js

--- src/script.js
+++
@@ -1,8 +0,0 @@
-// src/script.js
-
-const menuButton = document.getElementById('menuButton');
-const sidebar = document.getElementById('sidebar');
-
-menuButton.addEventListener('click', () => {
-  sidebar.classList.toggle('show-sidebar');
-});
